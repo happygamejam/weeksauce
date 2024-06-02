b39e42ae6022e90d967ee0c932c0c2a2e852dd9d
Alexy Mantha <alexy@mantha.dev>
2024-06-01 20:48:30 -0400

    setup Unreal

---
+++ Source/weeksauce/weeksauce.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "weeksauce.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, weeksauce, "weeksauce" );
