eb47ff91b3fc0b3aa7477cb653e6d007d7c03000
Alexy Mantha <alexy@mantha.dev>
2024-06-04 14:53:13 -0400

    fix: use seed in random generator
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- Assets/Scripts/DungeonParameters.cs
+++ Assets/Scripts/DungeonParameters.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DungeonParameters", menuName = "Scriptable Objects/DungeonParameters")]
 public class DungeonParameters : ScriptableObject
 {
+    // Seed for the random number generator
+    // If negative, a random seed will be used
     public int seed;
 
     // Number of rooms to generate
