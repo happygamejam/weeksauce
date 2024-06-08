8f4ae78e97b8695562b4aa676976bc46292aa278
Alexy Mantha <alexy@mantha.dev>
2024-06-08 17:18:26 -0400

    feat: spawn player (#10)
    Signed-off-by: Alexy Mantha <alexy@mantha.dev>

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -2,16 +2,7 @@ using UnityEngine;
 
 public abstract class Room : MonoBehaviour
 {
-   // Offset for the next room.
-   // This is the size of the room in the axis of the doors
-   private float offset;
-   public float Offset => offset;
-
    public abstract GameObject Generate(DungeonParameters parameters);
-
-   protected Room(float offset) {
-      this.offset = offset;
-   }
 }
 
 [System.Serializable]
